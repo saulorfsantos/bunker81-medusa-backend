36c4421b023bdbe2f631bcc65490ea87cda0c0b6
saulorfsantos <saulorfsantos13@gmail.com>
2026-07-05 21:19:40 -0300

    Add S3 file provider configuration to medusa-config

--- medusa-config.ts
+++ medusa-config.ts
@@ -22,6 +22,28 @@ module.exports = defineConfig({
   },
   modules: [
     {
+      resolve: "@medusajs/medusa/file",
+      options: {
+        providers: [
+          {
+            resolve: "@medusajs/file-s3",
+            id: "s3",
+            options: {
+              file_url: process.env.S3_FILE_URL,
+              endpoint: process.env.S3_ENDPOINT,
+              bucket: process.env.S3_BUCKET,
+              access_key_id: process.env.S3_ACCESS_KEY_ID,
+              secret_access_key: process.env.S3_SECRET_ACCESS_KEY,
+              region: "auto",
+              additional_client_config: {
+                forcePathStyle: true,
+              },
+            },
+          },
+        ],
+      },
+    },
+    {
       resolve: "@medusajs/medusa/cache-redis",
       options: {
         redisUrl: process.env.REDIS_URL,
